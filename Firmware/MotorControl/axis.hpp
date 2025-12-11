ce56618aeb5f7f170c19d05c1f3169d445509074
YangXiangOvobot <xiang.yang@ovobot.cc>
2025-12-11 18:04:35 +0800

    添加最大转速参数的读取设置及保存功能 (#14)
    * 添加自定义参数
    * 最大电机转速[转/秒]
    * 添加参数读取、设置、保存功能
    * Update 滚刷电机配置.txt

--- Firmware/MotorControl/axis.hpp
+++ Firmware/MotorControl/axis.hpp
@@ -58,6 +58,11 @@ public:
         uint32_t can_heartbeat_rate_ms = 100;
         uint16_t heartbeat_rate_ms = 30;    // 100ms units
         uint16_t current_threshold_mA = 4000; // Current threshold for M0 message in mA
+        uint16_t max_speed_limit = 50;// [turns/s], for step/dir input
+        uint16_t template_para1 = 0;
+        uint16_t template_para2 = 0;
+        uint16_t template_para3 = 0;
+        uint16_t template_para4 = 0; // reserved for future use
         // custom setters
         Axis* parent = nullptr;
         void set_step_gpio_pin(uint16_t value) { step_gpio_pin = value; parent->decode_step_dir_pins(); }
